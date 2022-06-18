3e82780622605e13a3f8e9d9c176dfdd74b4c0e0
Joesphson <Joesphson@king.com>
2022-06-18 21:01:48 +0800

    pushTest commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("have fixed");
         System.out.println("now I fixed too");
+        System.out.println("push test");
     }
 }
